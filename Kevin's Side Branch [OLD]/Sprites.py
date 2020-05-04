a7c91325c029f7f80bede9ab1947aec525cf70bc
kyeung7 <kevin.yeung.1@uvm.edu>
2020-05-04 19:47:06 -0400

    Reformatting

--- Kevin's Side Branch [OLD]/Sprites.py
+++
@@ -1,9 +0,0 @@
-# Team 9 RISK
-
-# Contains information on sprite layer and bounds
-class Sprites():
-    def __init__(self, surface, name_id):
-        self.layout = surface
-        self.name_pays = ""
-        self.id = int(name_id[-6: -4])
-        self.bounds = surface.get_bounding_rect()
